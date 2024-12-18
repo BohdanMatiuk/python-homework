3db1fd556379124efa958ac2949977cac079c3c7
BohdanMatiuk <mbs.dvp@gmail.com>
2024-12-18 18:47:41 +0000

    hw-2

--- homework2/hw2.2.py
+++ homework2/hw2.2.py
@@ -5,15 +5,24 @@ print()
 num2=float(input("Enter second number:  "))
 print()
 
+def add(num1, num2):
+    return num1+num2
+def sub(num1, num2):
+    return num1-num2
+def mult(num1, num2):
+    return num1*num2
+def div(num1, num2):
+    return num1/num2
+
 print("Result:")
 if operator=="+":
-    print(f"{num1} + {num2} =",round((num1+num2),3))
+    print(f"{num1} + {num2} =", round(add(num1,num2),3))
 elif operator=="-":
-    print(f"{num1} - {num2} =",round((num1-num2),3))
+    print(f"{num1} - {num2} =",round(sub(num1,num2),3))
 elif operator=="*":
-    print(f"{num1} x {num2} =",round((num1*num2),3))
+    print(f"{num1} x {num2} =",round(mult(num1,num2),3))
 elif operator=="/":
-    print(f"{num1} : {num2} =",round((num1/num2),3))
+    print(f"{num1} : {num2} =",round(div(num1,num2),3))
 else:
     print("choose '+', '-', '*' or '/' operator ")
 print()
